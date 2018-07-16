82a0b3932197f5bdb8c1b69a4902541d07bf64d2
samyak agrawal <samagrawal.197@gmail.com>
2018-07-16 11:36:48 +0530

    accuracy increased a bit

--- feature_format.py
+++ feature_format.py
@@ -1,13 +1,14 @@
 import tensorflow.python.platform
 import csv
+import sys
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder
-
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+np.set_printoptions(threshold=sys.maxint)
 
 def featureFormat(filename):
-    #List contains features to be selected from the csv file
+    # List contains features to be selected from the csv file
     features_list = ["Name", "Type 1", "Type 2",
                      "Total", "HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"]
     # Arrays to hold the labels and feature vectors.
@@ -17,11 +18,17 @@ def featureFormat(filename):
     # read the above columns from the csv file
     df = pd.read_csv(filename, usecols=features_list)
     labels = df[df.columns[1]]
-    features = df[df.columns[3:]].values
-    # print(features)
-    # print(df[df.columns[3:]])
+    # print(df[df.columns[1]])
+    features = df[df.columns[3:]].values.astype(float)
+    min_max_scaler = MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(features)
+    features = pd.DataFrame(x_scaled)
+    # features.plot(kind='bar')
+    # features = (features - features.mean()) / \
+    #     (features.max() - features.min())
     all_types = np.unique(labels)
-    #perform one hot encoding on labels
+    # print(all_types.shape)
+    # perform one hot encoding on labels
 
     encoder = LabelEncoder()
     encoder.fit(labels)
@@ -34,11 +41,8 @@ def featureFormat(filename):
     # all_types = np.transpose(np.matrix(all_types).astype(str))
     # # Convert the array of int labels into a numpy array.
     # labels_np = np.array(labels).astype(dtype=np.uint8)
-
-    # # Convert the int numpy array into a one-hot matrix.
-    # labels_onehot = (np.arange(NUM_LABELS) == labels_np[
-    #                  :, None]).astype(np.float32)
-    print(labels)
+    # print(labels)
+    # tf.Print(labels, [labels])
     return features, labels, all_types
 
 
--- softmax.py
+++ softmax.py
@@ -5,19 +5,17 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from sklearn.preprocessing import OneHotEncoder
-# Global variables.
-# num_labels = 2    # The number of labels.
-BATCH_SIZE = 100  # The number of training examples to use per training step.
+import sys
+import matplotlib.pyplot as plt
+np.set_printoptions(threshold=sys.maxint)
+
 
 # Define the flags useable from the command line.
 tf.app.flags.DEFINE_float('learningRate', None,
                           'File containing the training data (labels & features).')
-tf.app.flags.DEFINE_string('test', None,
-                           'File containing the test data (labels & features).')
 tf.app.flags.DEFINE_integer('num_epochs', 1,
                             'Number of examples to separate from the training '
                             'data for the validation set.')
-tf.app.flags.DEFINE_boolean('verbose', False, 'Produce verbose output.')
 FLAGS = tf.app.flags.FLAGS
 # print FLAGS.train
 # print FLAGS.test
@@ -26,8 +24,6 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def main(argv=None):
-    # Be verbose?
-    verbose = FLAGS.verbose
     num_epochs = FLAGS.num_epochs
     learningRate = FLAGS.learningRate
     features, labels, types = featureFormat("Pokemon.csv")
@@ -36,38 +32,13 @@ def main(argv=None):
     train_features, test_features, train_labels, test_labels = train_test_split(
         features, labels, test_size=0.2, random_state=42)
 
-    # Get the number of epochs and learning rate for training.
-    # This is where training samples and labels are fed to the graph.
-    # These placeholder nodes will be fed a batch of training data at each
-    # training step using the {feed_dict} argument to the Run() call below.
-    # print(features.shape[1])
     num_labels = len(types)
     num_features = features.shape[1]
     x = tf.placeholder("float", shape=[None, num_features])
     W = tf.Variable(tf.zeros([num_features, num_labels]))
     b = tf.Variable(tf.zeros([num_labels]))
     y_ = tf.placeholder("float", shape=[None, num_labels])
-    # print(tf.shape())
-    # z = tf.placeholder(tf.float32, shape=(1024, 1024))
-    # print(tf.get_shape(z))
-    # print(train_features.shape)
-    # For the test data, hold the entire dataset in one constant node.
-    # print(type(train_features))
 
-    # These are the weights that inform how much each feature contributes to
-    # the classification.
-    # print(tf.size(W))
-    # y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-    # # Optimization.
-    # cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-    # # print(type(cross_entropy))
-    # train_step = tf.train.GradientDescentOptimizer(
-    #     learningRate).minimize(cross_entropy)
-
-    # # Evaluation.
-    # correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    # accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     weights = {
         'h1': tf.Variable(tf.truncated_normal([num_features, 60])),
         'h2': tf.Variable(tf.truncated_normal([60, 60])),
@@ -90,6 +61,7 @@ def main(argv=None):
     saver = tf.train.Saver()
 
     y = multilayer_perceptron(x, weights, biases)
+    # y = _generateTensorLayers(x, weights, biases)
 
     cost_function = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
@@ -98,10 +70,9 @@ def main(argv=None):
 
     sess = tf.Session()
     sess.run(init)
-
     mse_history = []
     accuracy_history = []
-    cost_history = []
+    cost_history = np.empty(shape=[1], dtype=float)
 
     for epoch in range(num_epochs):
         sess.run(training_step, feed_dict={
@@ -110,24 +81,37 @@ def main(argv=None):
                         x: train_features, y_: train_labels})
         cost_history = np.append(cost_history, cost)
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        # tf.Print(y, [y])
+        # sess.run(y, feed_dict={x: train_features})
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         pred_y = sess.run(y, feed_dict={x: test_features})
         mse = tf.reduce_mean(tf.square(pred_y - test_labels))
-        mse_ = sess.run(mse)
-        mse_history.append(mse_)
+        mse_history.append(sess.run(mse))
         accuracy = (sess.run(accuracy, feed_dict={
                     x: train_features, y_: train_labels}))
         accuracy_history.append(accuracy)
 
         print('epoch: ', epoch, ' - cost: ', cost,
               " - MSE: ", mse, "- Train Accuracy: ", accuracy)
-
     save_path = saver.save(sess, model_path)
     print(" Model Saved in file: {}".format(save_path))
     pred_y = sess.run(y, feed_dict={x: test_features})
     mse = tf.reduce_mean(tf.square(pred_y - test_labels))
     print("MSE : {}".format(sess.run(mse)))
-    print(test_labels.shape)
+
+    # plt.plot(mse_history, "r")
+    # plt.show()
+    # plt.plot(accuracy_history)
+    # plt.show()
+
+    # correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # print("Test Accuracy: ", (sess.run(accuracy, feed_dict={x: test_features, y_: test_labels})))
+
+    # pred_y = sess.run(y, feed_dict={x: test_features})
+    # mse = tf.reduce_mean(tf.square(pred_y - test_labels))
+    # print("MSE: %.4f" % sess.run(mse))
+    # print(test_labels.shape)
 # Create a local session to run this computation.
     # with tf.Session() as s:
     #     # Run all the initializers to prepare the trainable parameters.
@@ -176,7 +160,7 @@ def main(argv=None):
 def multilayer_perceptron(x, weights, biases):
 
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.sigmoid(layer_1)
+    layer_1 = tf.nn.relu(layer_1)
 
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_2 = tf.nn.sigmoid(layer_2)
@@ -184,10 +168,11 @@ def multilayer_perceptron(x, weights, biases):
     layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
     layer_3 = tf.nn.sigmoid(layer_3)
 
-    layer_4 = tf.add(tf.matmul(layer_3, weights['h3']), biases['b4'])
+    layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
     layer_4 = tf.nn.relu(layer_4)
 
-    out_layer = tf.add(tf.matmul(layer_4, weights['out']), biases['out'])
+    out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
+    print "Hello"
     return out_layer
 
 
